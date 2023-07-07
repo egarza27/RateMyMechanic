e1b372e48046053c0b751ebf901cf419c55e2452
egarza27 <elizabethigarza10@gmail.com>
2023-07-06 19:15:30 -0500

    updated initialize sql file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-const axios = require("axios");
+import React from "react";
 
-const SerpApi = require("google-search-results-nodejs");
-const search = new SerpApi.GoogleSearch(
-  "8bb813d737db8e82d6ad594ee111d1a1435ce93c04a6573061863815565589e9"
-);
+// const axios = require("axios");
 
-const params = {
-  engine: "google_maps",
-  q: "mechanic shop",
-  ll: "@40.7455096,-74.0083012,15.1z",
-  type: "search",
-};
+// const SerpApi = require("google-search-results-nodejs");
+// const search = new SerpApi.GoogleSearch(
+//   "8bb813d737db8e82d6ad594ee111d1a1435ce93c04a6573061863815565589e9"
+// );
 
-const callback = function (data) {
-  console.log(data["local_results"]);
-};
+// const params = {
+//   engine: "google_maps",
+//   q: "mechanic shop",
+//   ll: "@40.7455096,-74.0083012,15.1z",
+//   type: "search",
+// };
 
-// Show result as JSON
-search.json(params, callback);
+// const callback = function (data) {
+//   console.log(data["local_results"]);
+// };
+
+// // Show result as JSON
+// search.json(params, callback);
